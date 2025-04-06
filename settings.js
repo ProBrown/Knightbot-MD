abe61519183004fadddeb528009b1d5ae0bf7e5d
ProBrown <163339408+ProBrown@users.noreply.github.com>
2025-04-06 21:48:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Limitcode',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "LIMITCODE",
+  botOwner: 'JADEN_KOBOS, // Your name
+  ownerNumber: '27835477507', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
